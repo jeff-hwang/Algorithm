3e0da2f43c4a69298e373e2ab89e19155e890c1b
YoonHoo Hwang <yoonhu1431@gmail.com>
2019-03-09 22:31:36 +0900

    2748. Fibonacci numbers.py

--- 189. Rotate Array.py
+++ 189. Rotate Array.py
@@ -5,9 +5,24 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        # 문제 설명
+        '''
+        nums 배열이 input으로 주어지는데
+        뒤에서 부터 k 개를 꺼내 앞으로 붙여라
 
-        
+        ex) nums = [1,2,3,4,5,6,7]
+            k = 3
+
+            step 1 :  [7,1,2,3,4,5,6]
+            step 2 :  [6,7,1,2,3,4,5]
+            step 3 :  [5,6,7,1,2,3,4]
+
+        '''
+        # nums 의 길이를 구다
         ln = len(nums)
+        
+        # k 번 반복하여 nums의 맨 마지막 원소를 꺼내
+        # 0번 인덱스에 삽입한다.
         for i in range(k):
             nums.insert(0, nums.pop(ln-1))      
         
