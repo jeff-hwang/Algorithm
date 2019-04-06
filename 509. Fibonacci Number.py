c0088207c3066c38a3c28ddf945104ab9a6b70a3
YoonHoo <yoonhu1431@gmail.com>
2019-04-06 23:25:01 +0900

    509. Fibonacci Number

---
+++ 509. Fibonacci Number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def fib(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        if N == 0:
+            return 0
+        if N == 1:
+            return 1
+
+        return self.fib(N-1) + self.fib(N-2)
