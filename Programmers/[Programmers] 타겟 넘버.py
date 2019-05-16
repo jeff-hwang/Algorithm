82cd31177937f570faf0455a0d84663ed7d57a29
yoonhoohwang <yoonhu1431@gmail.com>
2019-05-16 18:50:34 +0900

    타겟넘버

---
+++ Programmers/[Programmers] 타겟 넘버.py
@@ -0,0 +1,22 @@
+class StaticVar(object):
+    def __init__(self, target):
+        self.target = target
+        self.cnt = 0
+
+sv = None
+def solution(numbers, target):
+    global sv
+    sv = StaticVar(target)
+    targetDFS(numbers, 0)
+    return sv.cnt
+    
+
+def targetDFS(numbers, index):
+    if index <len(numbers):
+        numbers[index] *= 1        
+        targetDFS(numbers, index+1)
+
+        numbers[index] *= -1
+        targetDFS(numbers, index+1)
+    elif sum(numbers) == sv.target:
+        sv.cnt+=1
