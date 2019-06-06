5cc46c8b3bcdc254b371b823756bcfac39bde2df
yoonhoohwang <yoonhu1431@gmail.com>
2019-06-06 23:52:01 +0900

    backjoon 1517. 버블소트.py

---
+++ BackJoon/1517. 버블소트.py
@@ -0,0 +1,38 @@
+N = int(input())
+nums = list(map(int,input().split()))
+cnt = 0
+def merge_sort(list):
+    if len(list) <= 1:
+        return list
+    mid = len(list) // 2
+    leftList = list[:mid]
+    rightList = list[mid:]
+    leftList = merge_sort(leftList)
+    rightList = merge_sort(rightList)
+    return merge(leftList, rightList)
+
+def merge(left, right):
+    result = []
+    global cnt
+    
+    while len(left) > 0 or len(right) > 0:
+        if len(left) > 0 and len(right) > 0:
+            if left[0] <= right[0]:
+                result.append(left[0])
+                left = left[1:]
+            else:
+                result.append(right[0])
+                right = right[1:]
+                cnt+=1
+        elif len(left) > 0:
+            result.append(left[0])
+            left = left[1:]
+        elif len(right) > 0:
+            result.append(right[0])
+            right = right[1:]
+    return result
+
+merge_sort(nums)
+print(cnt)
+            
+
