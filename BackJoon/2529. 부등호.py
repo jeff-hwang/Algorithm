830d8e9f75a3cb71193e046b74eb170457569514
yoonhoohwang <yoonhu1431@gmail.com>
2019-06-09 23:58:15 +0900

    2529. 부등호

---
+++ BackJoon/2529. 부등호.py
@@ -0,0 +1,16 @@
+N = int(input())
+stack = input().split()
+
+MAX =  "".join([str(9-i) for i in range(N+1)])
+MIN = "".join([i for i in range(N+1)])
+
+resMX = 0
+resMin = 9876543210
+
+while (resMx<=resMin):
+    for i in range(N):
+        
+        if MAX[i] 
+    
+
+
